3b3fa863f78b2d0cef89fc2d057908118f025e45
Priyansh Arora <praro@microsoft.com>
2020-01-21 15:00:05 +0530

    On branch master 	new file:   lab1/Task1-execution.ps1 	new file:   lab1/task1.json 	new file:   lab1/validation/SOE-Lab1-VM-provisioning.ps1

---
+++ lab1/Task1-execution.ps1
@@ -0,0 +1,23 @@
+﻿Param(
+  [string] [Parameter(Mandatory=$true)] $alias
+
+)
+
+
+Write-Host 'Please log into Azure now' -foregroundcolor Green;
+
+$Subscription = (Get-azSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
+
+$sub=Select-azSubscription -SubscriptionName $Subscription.Name
+
+
+$hash1 = @{ alias = $alias}
+
+$alias = "praro2"
+
+#create image template
+New-AzResourceGroupDeployment -Name task1-vmimage -ResourceGroupName "praro2-vmimages-rg" -TemplateFile E:\Azure\ready20\task2\task1.json -TemplateParameterObject $hash1
+
+#create a hardened windows 2016 image
+az resource invoke-action --resource-group $($alias+'-vmimages-rg') --resource-type  Microsoft.VirtualMachineImages/imageTemplates -n praro2-task01 --action Run
+
---
+++ lab1/validation/SOE-Lab1-VM-provisioning.ps1
@@ -0,0 +1,16 @@
+﻿Param(
+  [string] [Parameter(Mandatory=$true)] $alias
+)
+
+$vmimagerg =$alias+'-vmimages-rg'
+
+Write-Host 'Please log into Azure now' -foregroundcolor Green;
+#Login-azAccount
+
+$Subscription = (Get-azSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
+
+$sub=Select-azSubscription -SubscriptionName $Subscription.Name
+
+
+New-azResourceGroupDeployment -Name "SOE-Lab1-VMDeployment" -ResourceGroupName $vmimagerg -TemplateUri 'https://raw.githubusercontent.com/prarolab/soephase2/master/task1/artefacts/lab1-soevm.json' -TemplateParameterObject @{"alias"=$alias}
+
