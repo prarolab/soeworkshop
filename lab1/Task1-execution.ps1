599b320cc283a401c52e133aa6a2b58f121511d4
Priyansh Arora <praro@microsoft.com>
2020-01-21 15:51:29 +0530

    modified:   lab1/Task1-execution.ps1

--- lab1/Task1-execution.ps1
+++ lab1/Task1-execution.ps1
@@ -13,11 +13,7 @@ $sub=Select-azSubscription -SubscriptionName $Subscription.Name
 
 $hash1 = @{ alias = $alias}
 
-#$alias = "praro2"
 
 #create image template
 New-AzResourceGroupDeployment -Name task1-vmimage -ResourceGroupName "$($alias+'-vmimages-rg')" -TemplateFile .\task1.json -TemplateParameterObject $hash1
-
-#create a hardened windows 2016 image
-az resource invoke-action --resource-group $($alias+'-vmimages-rg') --resource-type  Microsoft.VirtualMachineImages/imageTemplates -n $($alias+'-task01') --action Run
 
