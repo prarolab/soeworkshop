d13bfc4e5d8c3be0504b3ff4f500b9a5623fd518
Priyansh Arora <praro@microsoft.com>
2020-02-04 19:20:31 -0800

    new file:   lab1/task1-imagecreation.ps1

---
+++ lab1/task1-imagecreation.ps1
@@ -0,0 +1,21 @@
+﻿Param(
+  [string] [Parameter(Mandatory=$true)] $alias
+
+)
+Login-azAccount
+
+Write-Host 'Please log into Azure now' -foregroundcolor Green;
+az login
+
+$Subscription = (Get-azSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
+
+$sub=Select-azSubscription -SubscriptionName $Subscription.Name
+
+
+
+
+$subscriptionid = $sub
+
+az account set –s $subscriptionid
+az resource invoke-action --resource-group $($alias+'-vmimages-rg') --resource-type  Microsoft.VirtualMachineImages/imageTemplates -n $($alias+'-task01') --action Run
+
