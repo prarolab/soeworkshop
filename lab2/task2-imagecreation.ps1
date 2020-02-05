93f0964b4ec221e8101c3d94fd7230336700c24e
Priyansh Arora <praro@microsoft.com>
2020-02-04 23:33:58 -0800

    renamed:    lab2/lab2-execution.ps1 -> lab2/task2-execution.ps1 	new file:   lab2/task2-imagecreation.ps1 	renamed:    lab2/validation/VM-EastUS/SOE-Lab2-VM-provisioning.ps1 -> lab2/validation/VM-EastUS/SOE-Lab2-VM-provisioning-EastUS.ps1 	modified:   lab2/validation/VM-WestuS/SOE-Lab2-VM-provisioning-WestUS.ps1

---
+++ lab2/task2-imagecreation.ps1
@@ -0,0 +1,18 @@
+﻿Param(
+  [string] [Parameter(Mandatory=$true)] $alias
+
+)
+
+
+Write-Host 'Please log into Azure now' -foregroundcolor Green;
+az login
+
+$Subscription = (Get-azSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
+
+$sub=Select-azSubscription -SubscriptionName $Subscription.Name
+
+$subscriptionid = $Subscription.Id
+
+az account set –s $subscriptionid
+az resource invoke-action --resource-group $($alias+'-vmimages-rg') --resource-type  Microsoft.VirtualMachineImages/imageTemplates -n $($alias+'-task02') --action Run
+
--- lab2/validation/VM-WestuS/SOE-Lab2-VM-provisioning-WestUS.ps1
+++ lab2/validation/VM-WestuS/SOE-Lab2-VM-provisioning-WestUS.ps1
@@ -5,11 +5,11 @@
 $vmimagerg =$alias+'-vmimages-rg'
 
 Write-Host 'Please log into Azure now' -foregroundcolor Green;
-Login-AzureRmAccount
+Login-AzAccount
 
-$Subscription = (Get-AzureRmSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
+$Subscription = (Get-AzSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
 
-$sub=Select-AzureRmSubscription -SubscriptionName $Subscription.Name
+$sub=Select-AzSubscription -SubscriptionName $Subscription.Name
 
-New-AzureRmResourceGroupDeployment -Name "SOE-Lab1-VMDeployment" -ResourceGroupName $vmimagerg -TemplateFile https://msreadylabs.blob.core.windows.net/workshop/Lab2-SoeVM-WestUS.json -TemplateParameterObject @{"alias"=$alias}
+New-AzResourceGroupDeployment -Name "SOE-Lab2-VMDeployment" -ResourceGroupName $vmimagerg -TemplateFile https://msreadylabs.blob.core.windows.net/workshop/Lab2-SoeVM-WestUS.json -TemplateParameterObject @{"alias"=$alias}
 
