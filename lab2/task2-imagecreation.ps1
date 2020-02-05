f8ef79c68ed1bacab288beb04ee692d02707d6d3
prarolab <priyansh2689@gmail.com>
2020-02-05 11:47:32 -0800

    Update task2-imagecreation.ps1

--- lab2/task2-imagecreation.ps1
+++ lab2/task2-imagecreation.ps1
@@ -8,7 +8,6 @@ Write-Host 'Please log into Azure now' -foregroundcolor Green;
 az login
 
 $Subscription = (Get-azSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
-
-az account set –s $subscription.Id
+az account set -s $Subscription.Id
 az resource invoke-action --resource-group $($alias+'-vmimages-rg') --resource-type  Microsoft.VirtualMachineImages/imageTemplates -n $($alias+'-task02') --action Run
 
