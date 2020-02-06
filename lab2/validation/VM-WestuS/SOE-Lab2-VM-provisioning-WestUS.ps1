94d34b83ad5f751d4af3d8860f846e34cd88e359
prarolab <priyansh2689@gmail.com>
2020-02-05 18:13:00 -0800

    Update SOE-Lab2-VM-provisioning-WestUS.ps1

--- lab2/validation/VM-WestuS/SOE-Lab2-VM-provisioning-WestUS.ps1
+++ lab2/validation/VM-WestuS/SOE-Lab2-VM-provisioning-WestUS.ps1
@@ -10,6 +10,6 @@ Login-AzAccount
 $Subscription = (Get-AzSubscription) |Select Name, Id | Out-GridView -Title "Select Azure Subscription " -PassThru
 
 $sub=Select-AzSubscription -SubscriptionName $Subscription.Name
-
-New-AzResourceGroupDeployment -Name "SOE-Lab2-VMDeployment" -ResourceGroupName $vmimagerg -TemplateFile https://msreadylabs.blob.core.windows.net/workshop/Lab2-SoeVM-WestUS.json -TemplateParameterObject @{"alias"=$alias}
+''
+New-AzResourceGroupDeployment -Name "SOE-Lab2-VMDeployment" -ResourceGroupName $vmimagerg -TemplateFile "https://raw.githubusercontent.com/prarolab/soephase2/master/Task2/artefacts/lab2westus/lab2vm-westus.json" -TemplateParameterObject @{"alias"=$alias}
 
